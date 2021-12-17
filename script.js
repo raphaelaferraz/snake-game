c491c8a645d48f9ae2ac9f3e83675f835d538d6a
raphaelaferraz <raphaelagferraz2@gmail.com>
2021-12-17 20:30:10 -0300

    Iniciando os movimentos da cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 };
+let direction = 'right';
 
 function criarFundo() {
   context.fillStyle = '#256458';
@@ -19,5 +20,28 @@ function criarCobrinha() {
   }
 }
 
-criarFundo();
-criarCobrinha();
+function inicioDeJogo() {
+  criarFundo();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == 'right') snakeX += box;
+  if (direction == 'left') snakeX -= box;
+  if (direction == 'up') snakeY -= box;
+  if (direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  };
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(inicioDeJogo, 100);
+
+inicioDeJogo();
