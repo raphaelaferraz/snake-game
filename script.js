5f0efd6024ed2695fb8e38cda5810c4d78011501
raphaelaferraz <raphaelagferraz2@gmail.com>
2021-12-17 22:15:17 -0300

    Adicionando comentários no código

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let canvas = document.getElementById('snake');
+let canvas = document.getElementById('snake'); //element responsible for the animation of the game | pt-br: elemento responsável pela animação do jogo
 let context = canvas.getContext('2d');
 let box = 32;
-let snake = [];
+let snake = []; //the snake is created as a list, as it will consist of a series of coordinates that will be painted, forming the little squares. | pt-br: a cobrinha é criada como uma lista, pois ela será constituída por uma série de coordenadas que serão pintadas, formando os quadradinhos
 snake[0] = {
   x: 8 * box,
   y: 8 * box
@@ -12,12 +12,12 @@ let food = {
   y: Math.floor(Math.random() * 15 + 1) * box
 };
 
-function criarFundo() {
+function createBackground() {
   context.fillStyle = '#256458';
-  context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillRect(0, 0, 16 * box, 16 * box); //creates the rectangle through x, y, width and height | pt-br: cria o retângulo através do x, y, largura e altura
 }
 
-function criarCobrinha() {
+function createSnake() {
   for (i = 0; i < snake.length; i++) {
     context.fillStyle = 'green';
     context.fillRect(snake[i].x, snake[i].y, box, box);
@@ -29,8 +29,10 @@ function drawFood() {
   context.fillRect(food.x, food.y, box, box);
 }
 
+//responsible for capturing all events, in this case, all keyboard events | pt-br: responsável por captar todos os acontecimentos, neste caso, todos os acontecimentos do teclado
 document.addEventListener('keydown', update);
 
+//responsible for attributing the movements to the snake | pt-br: responsável por atribuir os movimentos à cobrinha
 function update(event) {
   if (event.keyCode == 37 && direction != 'right') direction = 'left';
   if (event.keyCode == 38 && direction != 'down') direction = 'up';
@@ -38,12 +40,14 @@ function update(event) {
   if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function inicioDeJogo() {
+function gameStart() {
+  //conditionals that allow the snake to "cross" the set space of the animation | pt-br: condicionais que permitem que a cobrinha "atravesse" o espaço setado da animação
   if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
   if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
   if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+  //loop responsible for defining when the snake's size increases or remains the same, and when the game is over | pt-br: laço responsável por definir quando a tamanho da cobrinha aumenta ou permanece o mesmo, e quando o jogo acaba
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       clearInterval(game);
@@ -51,8 +55,8 @@ function inicioDeJogo() {
     }
   }
 
-  criarFundo();
-  criarCobrinha();
+  createBackground();
+  createSnake();
   drawFood();
 
   let snakeX = snake[0].x;
@@ -63,6 +67,7 @@ function inicioDeJogo() {
   if (direction == 'up') snakeY -= box;
   if (direction == 'down') snakeY += box;
 
+  //conditional responsible for making the snake's food appear in random places | pt-br: condicional responsável por fazer com que a comida da cobrinha apareça em lugares aleatórios
   if (snakeX != food.x || snakeY != food.y) {
     snake.pop();
   } else {
@@ -70,6 +75,7 @@ function inicioDeJogo() {
     food.y = Math.floor(Math.random() * 15 + 1) * box;
   }
 
+  //variable responsible for creating the snake's head | pt-br: variável responsável por criar a cabeça da cobrinha
   let newHead = {
     x: snakeX,
     y: snakeY
@@ -78,6 +84,6 @@ function inicioDeJogo() {
   snake.unshift(newHead);
 }
 
-let game = setInterval(inicioDeJogo, 100);
+let game = setInterval(gameStart, 100);
 
-inicioDeJogo();
+gameStart();
